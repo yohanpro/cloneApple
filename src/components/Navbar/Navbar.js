9b2a8b678c20a2b2c6f9213fd4d52c408850f9bb
Yohan <yohan@yohanpro.com>
2019-08-08 20:44:34 +0900

    메뉴버튼 클릭시 x 전환 애니메이션 추가

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,40 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.scss";
+import classnames from "classnames";
 
-const Navbar = class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  changeMeunIcon() {
-    console.log(this);
-  }
-  render() {
-    return (
-      <nav
-        id="globalNav"
-        className={styles.navbar__global}
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <ul className={styles["navbar__global-content"]}>
-          <li
-            className={styles.navbar__menuIcon}
-            onClick={e => this.changeMeunIcon(e)}
-          >
-            <div className={styles["menuIcon-top"]} />
-            <div className={styles["menuIcon-bottom"]} />
-          </li>
-          <li className={styles.navbar__appleIcon}>
-            <a />
-          </li>
-          <li className={styles.navbar__shopIcon}>
-            <a />
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+const Navbar = () => {
+  let [expanded, setMenuState] = useState(false);
+  const changeMeunIcon = e => {
+    expanded = !expanded;
+    setMenuState(expanded);
+  };
+  return (
+    <nav
+      id="globalNav"
+      className={styles.navbar__global}
+      role="navigation"
+      aria-label="main-navigation"
+    >
+      <ul className={styles["navbar__global-content"]}>
+        <li className={classnames("menuIcon")} onClick={e => changeMeunIcon(e)}>
+          <div className={classnames("menuIcon-top", { expanded: expanded })} />
+          <div
+            className={classnames("menuIcon-bottom", { expanded: expanded })}
+          />
+        </li>
+        <li className={styles.navbar__appleIcon}>
+          <a />
+        </li>
+        <li className={styles.navbar__shopIcon}>
+          <a />
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
